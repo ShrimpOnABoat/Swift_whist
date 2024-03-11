fc517364362b6ab0b572a05d39871cc78d9f05bc
Tony Buffard <tony.buffard@vidensanalytics.com>
2024-03-11 17:55:00 -0400

    Initial Commit

---
+++ Whist/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Whist
+//
+//  Created by Tony Buffard on 2024-03-11.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
